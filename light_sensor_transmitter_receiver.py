934c28fd50d69f4af36c3a019105587ee1316362
Vincent Chong <ws.chong.sg@gmail.com>
2021-08-30 22:06:32 +0800

    Update light_sensor_transmitter_receiver.py

--- light_sensor_transmitter_receiver.py
+++ light_sensor_transmitter_receiver.py
@@ -1,1 +1,22 @@
+#board1 triggers board2 to transmit board2 light sensor reading to board1
+#board1 then send the received value to data streamer
+#for board1 to detect light in a darkbox that store board2
+def on_received_number(receivedNumber):
+    dataStreamer.write_number(receivedNumber)
+    dataStreamer.write_line()
+    basic.show_number(receivedNumber)
+radio.on_received_number(on_received_number)
 
+def on_button_pressed_a():
+    radio.send_string("A")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+    radio.send_number(input.light_level())
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
+radio.on_received_string(on_received_string)
+
+radio.set_group(22)
+dataStreamer.set_baud_rate(BaudRate.BAUD_RATE9600)
+basic.show_icon(IconNames.HEART)
